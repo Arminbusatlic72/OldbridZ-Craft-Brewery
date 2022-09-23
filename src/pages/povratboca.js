5360303c1c9789b64f3ce6ddc65c11002e48e1de
armin busatlic <dzarma.busatlic@gmail.com>
2022-09-23 16:22:03 +0200

    Adding beer ticket images to second page

--- src/pages/povratboca.js
+++ src/pages/povratboca.js
@@ -1,14 +1,36 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
+    <h1>Naša piva</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <div className={styles.textCenter}>
+      <StaticImage
+        src="../images/Ticket2.jpg"
+        loading="eager"
+        width={800}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt=""
+        style={{ marginBottom: `var(--space-3)` }}
+      />
+      <StaticImage
+        src="../images/ticket1.jpg"
+        loading="eager"
+        width={800}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt=""
+        style={{ marginBottom: `var(--space-3)` }}
+      />
+    </div>
   </Layout>
 )
 
